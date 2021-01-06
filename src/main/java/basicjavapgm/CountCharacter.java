359fad082ae45af5aa23a73a50ae6ef9cd1723fb
AbubakerSiddiq <abubakkarsiddiq1989@gmail.com>
2021-01-06 22:58:50 +0530

    using foreach    1.taken count of character    2.and count wihout space

--- src/main/java/basicjavapgm/CountCharacter.java
+++ src/main/java/basicjavapgm/CountCharacter.java
@@ -3,13 +3,29 @@ package basicjavapgm;
 public class CountCharacter{
 
 	public static void main(String[] args) {
-String s="Rafan is sleeping on the bed";
+		String s="Rafan is sleeping on the bed";
 
-int count=s.length();
-/*
- * for (int i = 0; i < splits.length; i++) { String word=splits[i];
- * word.charAt(i).; }
- */
-System.out.println("number of character is " +count);
+		int count=s.length();
+		/*
+		 * for (int i = 0; i < splits.length; i++) { String word=splits[i];
+		 * word.charAt(i).; }
+		 */
+		System.out.println("number of character is " +count);
+
+		char[] ch = s.toCharArray();
+		int value=0;
+		for (char c : ch) {
+			value++;
+
+		}
+		System.out.println("using for loop"+value);
+		
+		int withOutSpace=0;
+		for (char c : ch) {
+			if (c!=' ') {
+				withOutSpace++;
+			}
+		}
+		System.out.println(withOutSpace);
 	}
 }
