3eac6d7f4f2469d125b077269f1343d8de4f229a
Abu <abubakersiddiqm@gmail.com>
2022-06-17 00:28:57 +0530

    Arraypresentinodd

--- src/main/java/arrayPrgrams/ArrayPresentOnOddPosition.java
+++ src/main/java/arrayPrgrams/ArrayPresentOnOddPosition.java
@@ -7,7 +7,9 @@ public class ArrayPresentOnOddPosition {
 		int a[] = { 1, 2, 3, 4, 5, 6 };
 		for (int i = 0; i < a.length; i++) {
 			if(i%2==0){
-			System.out.println(a[i]);
+
+				System.out.println(a[i]);
+
 			}
 		}
 	}	
