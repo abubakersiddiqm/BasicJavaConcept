33a8f8ddb9461b7451dfe9ec29ab83aee751626b
Abu <abubakersiddiqm@gmail.com>
2022-06-17 00:21:02 +0530

    ArrayPresentOnOddPosition code change

--- src/main/java/arrayPrgrams/ArrayPresentOnOddPosition.java
+++ src/main/java/arrayPrgrams/ArrayPresentOnOddPosition.java
@@ -5,8 +5,10 @@ public class ArrayPresentOnOddPosition {
 	public static void main(String[] args) {
 
 		int a[] = { 1, 2, 3, 4, 5, 6 };
-		for (int i = 0; i < a.length; i = i + 2) {
+		for (int i = 0; i < a.length; i++) {
+			if(i%2==0){
 			System.out.println(a[i]);
+			}
 		}
 	}	
 }
